bf65e43d477b0ef94e8688a5232a979e52b688e1
Saul.Bravo <sauluriel1402@gmail.com>
2022-05-11 21:14:57 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const app = express();
+
+let port = 8080;
+
+app.use(bodyParser);
+app.use(express.json());
+app.use(cors());
+app.all((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS, PUT");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    next();
+});
+
+app.get("/get", function (req, res) {
+    res.send({
+        hello: "world"
+    });
+});
+
+app.listen(port, () => {
+    console.log("listening port: " + port);
+});
